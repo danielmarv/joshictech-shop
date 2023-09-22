0b9758c53febbadd78b9708fac8b230d559d4d72
Daniel Ntege <danientege785@gmail.com>
2023-09-22 19:55:15 +0300

    Update CategoryController.php

--- backend/app/Http/Controllers/Api/V2/Category/CategoryController.php
+++ backend/app/Http/Controllers/Api/V2/Category/CategoryController.php
@@ -1,1 +1,69 @@
 <?php
+// app/Http/Controllers/CategoryController.php
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json(['categories' => $categories], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'description' => 'string',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return response()->json(['category' => $category], 201);
+    }
+
+    public function show($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        return response()->json(['category' => $category], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $this->validate($request, [
+            'name' => 'string',
+            'description' => 'string',
+        ]);
+
+        $category->update($request->all());
+
+        return response()->json(['category' => $category], 200);
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $category->delete();
+
+        return response()->json(['message' => 'Category deleted'], 204);
+    }
+}
