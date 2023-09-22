fe9296ff69e398bb3556b3b3bd00932f6808e263
Daniel Ntege <danientege785@gmail.com>
2023-09-22 19:54:48 +0300

    Commit

---
+++ backend/app/Http/Controllers/Api/V2/Product/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+// app/Http/Controllers/ProductController.php
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json(['products' => $products], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'description' => 'string',
+            'price' => 'required|numeric',
+            'quantity' => 'required|integer',
+            'category' => 'string',
+            'image' => 'string',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json(['product' => $product], 201);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json(['product' => $product], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $this->validate($request, [
+            'name' => 'string',
+            'description' => 'string',
+            'price' => 'numeric',
+            'quantity' => 'integer',
+            'category' => 'string',
+            'image' => 'string',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json(['product' => $product], 200);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted'], 204);
+    }
+}
